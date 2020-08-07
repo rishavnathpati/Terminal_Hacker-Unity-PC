645aa5923a185ef8bb2e29fbd5b63a717b6064be
Rishav Nath Pati <patirishavnath@gmail.com>
2020-08-07 23:52:50 +0530

    Update Hacker.cs

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
@@ -7,7 +8,10 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Terminal.WriteLine("Hello! Welcome Hacker");
+        Terminal.WriteLine("************Welcome Hacker************\nWhat would you like to hack?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the local police station");
+        Terminal.WriteLine("Enter your choice");
     }
 
     // Update is called once per frame
